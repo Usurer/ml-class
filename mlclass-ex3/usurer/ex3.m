7e7ad654fe6eadd7f061d3d0a338907b5ebaa188
Usurer <ivvlg@narod.ru>
2018-10-03 13:46:58 +0300

    no message

--- mlclass-ex3/usurer/ex3.m
+++ mlclass-ex3/usurer/ex3.m
@@ -60,6 +60,7 @@ fprintf('\nTraining One-vs-All Logistic Regression...\n')
 
 lambda = 0.1;
 [all_theta] = oneVsAll(X, y, num_labels, lambda);
+whos
 
 fprintf('Program paused. Press enter to continue.\n');
 # pause;
